87b6d40f04b5743b021bc2f1d53ce6cd6521cee8
George Kankava <gkankava@uplandsoftware.com>
2017-12-14 15:14:30 +0400

    first commit

---
+++ src/main/java/com/gngapps/ovs/OvsApplication.java
@@ -0,0 +1,12 @@
+package com.gngapps.ovs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvsApplication.class, args);
+	}
+}
